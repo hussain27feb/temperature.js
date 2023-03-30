a1ceb29561c7dc87a9151001ae992d1eaed50b2d
hussain27feb <126004604+hussain27feb@users.noreply.github.com>
2023-03-30 14:24:28 -0700

    Create temmp.js

---
+++ temmp.js
@@ -0,0 +1,8 @@
+do {
+    number = prompt("Enter a number");
+    if (number % 2 == 0) {
+      alert(`${number} is even.`);
+    } else {
+      alert(`${number}  is odd.`);
+    }
+  } while (confirm("Do you want to enter another number?"))
